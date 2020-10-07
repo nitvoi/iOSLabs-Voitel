28fa37a36db1ba379bb206b7ff71d9e138608486
JustinVoitel <justin.voitel@gmail.com>
2020-10-07 15:25:59 +0200

    init

---
+++ iOS1-Voitel/iOS1-Voitel/Vector.swift
@@ -0,0 +1,14 @@
+//
+//  Vector.swift
+//  iOS1-Voitel
+//
+//  Created by  on 07.10.20.
+//  Copyright © 2020 voitel. All rights reserved.
+//
+
+import Foundation
+
+
+class Vector{
+    var x = 0.0
+}
---
+++ iOS1-Voitel/iOS1-Voitel/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  iOS1-Voitel
+//
+//  Created by  on 07.10.20.
+//  Copyright © 2020 voitel. All rights reserved.
+//
+
+import Foundation
+
+print("Bitte geben Sie ihren Namen ein")
+
+
+let answer = readLine()
+
+if let _answer = answer{
+    print("Hallo \(_answer)")
+}
