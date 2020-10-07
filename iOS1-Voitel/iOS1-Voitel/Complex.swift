97bc4aa6a6faaad9de72ef6eaa3ee2c4d938dae4
JustinVoitel <justin.voitel@gmail.com>
2020-10-07 17:33:25 +0200

    add first lab

---
+++ iOS1-Voitel/iOS1-Voitel/Complex.swift
@@ -0,0 +1,47 @@
+//
+//  Complex.swift
+//  iOS1-Voitel
+//
+//  Created by  on 07.10.20.
+//  Copyright © 2020 voitel. All rights reserved.
+//
+
+import Foundation
+class Complex {
+    var r: Double
+    var i:Double
+    init(r:Double,i:Double){
+        self.r = r
+        self.i = i
+    }
+    public func incrementBy(r:Double) {
+        self.r += r
+    }
+    public func incrementBy(i:Double){
+        self.i += i
+    }
+    public func incrementBy(c:Complex){
+        self.r += c.r
+        self.i += c.i
+    }
+    public func multiplyBy(c:Complex){
+        let firsts = self.r * c.r
+        let outers = self.r * c.i
+        let inners = self.i * c.r
+        let lasts = self.i * c.i * -1
+        self.r = firsts + lasts
+        self.i = outers + inners
+    }
+    public func rotateBy(angle:Double){
+        //TODO
+        //(cos(p) + sin(p) i)
+    }
+    public func plus(c:Complex)->Complex{
+        let comp = Complex(r:self.r,i:self.i)
+        comp.incrementBy(c: c)
+        return comp
+    }
+    public func printString(){
+        print("\(self.r) \(self.i >= 0 ? "+":"-") \(sqrt(pow(self.i,2))) i")
+    }
+}
--- iOS1-Voitel/iOS1-Voitel/main.swift
+++ iOS1-Voitel/iOS1-Voitel/main.swift
@@ -8,11 +8,51 @@
 
 import Foundation
 
-print("Bitte geben Sie ihren Namen ein")
+// 1.
+print("Bitte geben Sie den Realteil für eine Komplexe Zahl ein!")
+var re = readDouble()
+
+print("Bitte geben Sie den Imaginärteil für eine Komplexe Zahl ein!")
+var im = readDouble()
+
+var c = Complex(r:re,i:im)
+c.printString()
+
+//2.
+c.incrementBy(c:Complex(r:1.2, i:2.4))
+c.printString()
+
+//3.
+c.incrementBy(r:2.2)
+c.printString()
+
+//4
+c.incrementBy(i:0.6)
+c.printString()
+
+//5.
+c.multiplyBy(c:Complex(r:0.5, i:1.0))
+c.printString()
+
+//6.
+c.rotateBy(angle:Double.pi)
+c.printString()
+
+//7.
+var c2 = c.plus(c:Complex(r:0.2, i: 0.9))
+c2.printString()
 
 
-let answer = readLine()
-
-if let _answer = answer{
-    print("Hallo \(_answer)")
+func readDouble()-> Double{
+    let input = readLine()
+    if let _input = input {
+        let double = Double(_input)
+        if let _double = double{
+            return _double
+        }else{
+            return 0.0
+        }
+    }else{
+        return 0.0
+    }
 }
